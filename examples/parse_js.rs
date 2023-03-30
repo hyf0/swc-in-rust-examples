9a60558500eb352600d25cbbe4ff3045af1e67c0
YunfeiHe <i.heyunfei@gmail.com>
2023-03-30 21:41:22 +0800

    feat: Add parsing support for JavaScript files in library

---
+++ examples/parse_js.rs
@@ -0,0 +1,11 @@
+use swc_core::{ecma::{parser, ast}, common};
+
+fn main() {
+    let a_js = std::fs::read_to_string("./fixtures/js/a.js").unwrap();
+    let cm = common::SourceMap::default();
+    let fm = cm.new_source_file(swc_core::common::FileName::Anon, a_js);
+    let module = parser::parse_file_as_module(&fm, parser::Syntax::Es(parser::EsConfig {
+        ..Default::default()
+    }), ast::EsVersion::latest(), None, &mut vec![]).unwrap();
+    println!("module: {module:#?}");
+}
